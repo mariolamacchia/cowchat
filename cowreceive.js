fbcfdc505447565b31ff1c0a9c4c62d2cf277451
Mario Lamacchia <mariolamacchia@gmail.com>
2014-11-02 00:04:31 +0100

    Add cowsay to receive script

--- cowreceive.js
+++ cowreceive.js
@@ -1,7 +1,11 @@
 var socket = require('socket.io-client')('http://localhost:3000');
+var exec = require('child_process').exec;
+
 socket.on('connect', function(){
   socket.on('message', function(data){
-    console.log(data.content);
+    exec('cowsay ' + data.content, function(error, stdout, stderr) {
+      console.log(stdout);
+    });
   });
   socket.on('disconnect', function(){});
 });

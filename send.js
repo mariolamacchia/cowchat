a47732684f439db4533d0f2e9c8b9b87d007c27e
Mario Lamacchia <mariolamacchia@gmail.com>
2014-11-02 10:11:19 +0100

    send.js: fix typo

--- send.js
+++ send.js
@@ -2,8 +2,8 @@ var socket = require('socket.io-client')('http://localhost:3000');
 
 var date = Date.now();
 
-exports = function() {
-  socket.on('connect', function(content) {
+module.exports = function(content) {
+  socket.on('connect', function() {
     socket.on('message', function(data) {
       if (data.date == date) process.exit();
     });

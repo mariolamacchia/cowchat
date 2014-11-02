e789a16827823ca7ade3d3839a6a9d32f7deeef9
Mario Lamacchia <mariolamacchia@gmail.com>
2014-11-02 09:58:13 +0100

    Create index from main script

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-#!/usr/bin/env node
 var send = require('./send');
 var forever = require('forever');
 
 var command = process.argv[2];
 
 if (command === 'start') {
+
 } else if (command === 'stop') {
 } else if (command === 'send') {
 }

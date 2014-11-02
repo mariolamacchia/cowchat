95787a9d70e9a71701a2ec508cec47cd1efbc4d8
Mario Lamacchia <mariolamacchia@gmail.com>
2014-11-02 09:54:55 +0100

    cowsend: modularize

--- cowsend.js
+++ cowsend.js
@@ -2,9 +2,11 @@ var socket = require('socket.io-client')('http://localhost:3000');
 
 var date = Date.now();
 
-socket.on('connect', function() {
-  socket.on('message', function(data) {
-    if (data.date == date) process.exit();
+exports = function() {
+  socket.on('connect', function(content) {
+    socket.on('message', function(data) {
+      if (data.date == date) process.exit();
+    });
+    socket.emit('message', {date: date, content: content});
   });
-  socket.emit('message', {date: date, content: process.argv[2]});
-});
+}

c54640520bd18ad50cd713ee31c8a2e93f033fba
Mario Lamacchia <mariolamacchia@gmail.com>
2014-11-02 23:43:37 +0100

    messageId: fix typo

--- app/messageId.js
+++ app/messageId.js
@@ -4,7 +4,7 @@ module.exports = function createSessionId() {
       2 byte pid
       2 byte random value
   */
-  var id = ("00000000" + (((Date.now() / 1000) & 0xffffffff) >>> 0).toString(16)).substr(-8) +
+  return ("00000000" + (((Date.now() / 1000) & 0xffffffff) >>> 0).toString(16)).substr(-8) +
     ("0000" + (process.pid & 0xffff).toString(16)).substr(-4) +
     ("0000" + (Math.floor(Math.random() * 0x10000)).toString(16)).substr(-4);
 }
